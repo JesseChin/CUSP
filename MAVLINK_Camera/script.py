9811949e2bf1c6eb445829e69a9a78b693f98fe8
JeyaI <jeyaiyadurai@ufl.edu>
2023-01-27 15:31:36 -0500

    removed enum class for python 3.9.2

--- MAVLINK_Camera/script.py
+++ MAVLINK_Camera/script.py
@@ -1,14 +1,13 @@
 from pymavlink import mavutil
 import time
 
-class PROG_STATE(Enum):
-    INIT = 0
-    IDLE = 1
-    SEND = 2
+STATE_INIT = 0
+STATE_IDLE = 1
+STATE_SEND = 2
 
 
 if __name__ == "__main__":
-    state = PROG_STATE.INIT
+    state = STATE_INIT
     
     connection = mavutil.mavlink_connection('/dev/ttyAMA0', baud=57600)
     print("Creating connection...")
@@ -20,7 +19,7 @@ if __name__ == "__main__":
     connection.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_CAMERA, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
     
     while(True):
-        if(state == PROG_STATE.INIT):
+        if(state == STATE_INIT):
             
             msg = connection.recv_match(blocking=True)
         
@@ -34,13 +33,13 @@ if __name__ == "__main__":
                     if msg.get_type() == "MAV_CMD_REQUEST_MSG" and msg.param1 == 259 :
                         connection.mav.command_ack_send() #need params
                         connection.mav.camera_information_send()
-                        state = PROG_STATE.IDLE
+                        state = STATE_IDLE
                       
                     #Message is valid
                     # Use the attribute
                     print('Mode: %s' % msg.mode)
             
-        elif(state == PROG_STATE.IDLE):
+        elif(state == STATE_IDLE):
                 
             msg = connection.recv_match(blocking=True)
             
@@ -54,14 +53,14 @@ if __name__ == "__main__":
                     if msg.get_type() == "MAV_CMD_REQUEST_MSG" and msg.param1 == 262 :
                         connection.mav.mav_result_accepted_send() #need params
                             
-                        state = PROG_STATE.SEND
+                        state = STATE_SEND
             
             
             #TODO: wait for MAV_CMD_REQUEST_MSG with param1 = 262
             # implement relative portion of the camera protocol and move into SEND state
             
             
-        elif(state == PROG_STATE.SEND):
+        elif(state == STATE_SEND):
             pass
                 #
                 # LOGIC FOR TAKING PHOTO HERE

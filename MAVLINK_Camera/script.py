8db2ad467bb781a9e39564afa214df9ffe172fdf
JeyaI <jeyaiyadurai@ufl.edu>
2023-01-27 15:23:13 -0500

    removed switch statement for python 3.9.2

--- MAVLINK_Camera/script.py
+++ MAVLINK_Camera/script.py
@@ -20,37 +20,36 @@ if __name__ == "__main__":
     connection.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_CAMERA, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
     
     while(True):
-        match state:
-            case PROG_STATE.INIT:
+        if(state == PROG_STATE.INIT):
             
-                msg = connection.recv_match(blocking=True)
+            msg = connection.recv_match(blocking=True)
         
-                if not msg:
-                    #return
-                if msg.get_type() == "BAD_DATA":
-                    if mavutil.all_printable(msg.data):
-                        sys.stdout.write(msg.data)
-                        sys.stdout.flush()
+            if not msg:
+                #return
+            if msg.get_type() == "BAD_DATA":
+                if mavutil.all_printable(msg.data):
+                    sys.stdout.write(msg.data)
+                    sys.stdout.flush()
                 else:
                     if msg.get_type() == "MAV_CMD_REQUEST_MSG" and msg.param1 == 259 :
                         connection.mav.command_ack_send() #need params
                         connection.mav.camera_information_send()
                         state = PROG_STATE.IDLE
                       
-            #Message is valid
-            # Use the attribute
-            print('Mode: %s' % msg.mode)
+                    #Message is valid
+                    # Use the attribute
+                    print('Mode: %s' % msg.mode)
             
-            case PROG_STATE.IDLE:
+        elif(state == PROG_STATE.IDLE):
                 
-                msg = connection.recv_match(blocking=True)
+            msg = connection.recv_match(blocking=True)
             
-                    if not msg:
-                        #return
-                    if msg.get_type() == "BAD_DATA":
-                        if mavutil.all_printable(msg.data):
-                            sys.stdout.write(msg.data)
-                            sys.stdout.flush()
+                if not msg:
+                    #return
+                if msg.get_type() == "BAD_DATA":
+                    if mavutil.all_printable(msg.data):
+                        sys.stdout.write(msg.data)
+                        sys.stdout.flush()
                     else:
                         if msg.get_type() == "MAV_CMD_REQUEST_MSG" and msg.param1 == 262 :
                             connection.mav.mav_result_accepted_send() #need params
@@ -62,7 +61,7 @@ if __name__ == "__main__":
             # implement relative portion of the camera protocol and move into SEND state
             
             
-            case PROG_STATE.SEND:
+        elif(state == PROG_STATE.SEND):
                 #
                 # LOGIC FOR TAKING PHOTO HERE
                 #

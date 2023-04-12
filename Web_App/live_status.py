7bdced32b465b248c292fe0146dd1f83720145f2
shudgit <shudouglas@ufl.edu>
2023-04-12 16:18:47 -0400

    Added Prototypes

--- Web_App/live_status.py
+++ Web_App/live_status.py
@@ -1,14 +1,25 @@
 import os
 statvfs = os.statvfs(os.getcwd())
 
+def connected():
+    output = True
+    return output
+
 def storage():
     output = str(round(statvfs.f_bavail * statvfs.f_frsize * 1e-9, 2)) + " out of " + str(round(statvfs.f_blocks * statvfs.f_frsize * 1e-9, 2)) + " GB"
     return output
 
-def other():
+def sats():
     output = 5
     return output
 
-def connected():
-    output = True
-    return output
+def location():
+
+def altitude():
+
+def speed():
+
+def heading():
+
+def capture_config():
+
